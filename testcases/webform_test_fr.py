fc1660e67a4d93a32614c62d3dca4b35ecfa0f34
sairaj.kakodkar@sjinnovation.com <sairaj.kakodkar@sjinnovation.com>
2023-12-13 22:25:27 +0530

    pushed scenarios for hreflang

--- testcases/webform_test_fr.py
+++ testcases/webform_test_fr.py
@@ -23,20 +23,20 @@ testdata_form = "./test_data/form_data.csv"
 #     action_obj.verify_current_url(expected_partial_url)
 #     page.close()
 
-@pytest.mark.webform
-@pytest.mark.parametrize("url", config.Config.URLs_to_test_fr)
-def test_page_title_fr(url, page: Page) -> None:
-    page.set_default_timeout(80000)
-    page.goto(url)
-    data = reader.read_test_data(testdata, Action.get_current_test_name())
-    page_title = data[1]
-    webform_obj = Webform(page)
-    webform_obj.verify_page_title(page_title)
+# @pytest.mark.webform
+# @pytest.mark.parametrize("url", config.Config.URLs_to_test_fr)
+# def test_page_title_fr(url, page: Page) -> None:
+#     page.set_default_timeout(80000)
+#     page.goto(url)
+#     data = reader.read_test_data(testdata, Action.get_current_test_name())
+#     page_title = data[1]
+#     webform_obj = Webform(page)
+#     webform_obj.verify_page_title(page_title)
 
 @pytest.mark.webform
 @pytest.mark.parametrize("url", config.Config.URLs_to_test_fr)
 def test_meta_description(url, page: Page) -> None:
-    page.set_default_timeout(80000)
+    page.set_default_timeout(100000)
     page.goto(url)
     data = reader.read_test_data(testdata, Action.get_current_test_name())
     meta_fr = data[6]
@@ -254,4 +254,15 @@ def test_meta_description(url, page: Page) -> None:
 #     webform_obj.webform_form(firstname, emailid, verify_email, "", "verify_email")
 #     webform_obj.submit_button()
 #     webform_obj.email_address_error_check(email_error_text)
-#     page.close()     
+#     page.close()
+
+@pytest.mark.webform
+@pytest.mark.parametrize("url", config.Config.URLs_to_test_fr_thankyou_page)
+def test_href_lang(url, page: Page) -> None:
+    page.set_default_timeout(100000)
+    page.goto(url)
+    data = reader.read_test_data(testdata, Action.get_current_test_name())
+    href_en, href_fr = data[4], data[5]
+    webform_obj = Webform(page)
+    webform_obj.check_href_lang("EN", href_en)
+    webform_obj.check_href_lang("FR", href_fr)     
